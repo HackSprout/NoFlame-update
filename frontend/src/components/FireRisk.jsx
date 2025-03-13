e11226a36b579774ada8ad40642450b8843eef74
sneha-grg <snehagrg14@gmail.com>
2025-03-13 03:48:25 -0700

    Fire Animation implemented

--- frontend/src/components/FireRisk.jsx
+++ frontend/src/components/FireRisk.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './FireRisk.css';
 
-function FireRisk() {
+const FireRisk = () => {
   return (
-    <div className='container'>
+    <div className='section'>
       <div className='FireLikelihood'>
         ummmmmm
       </div>
